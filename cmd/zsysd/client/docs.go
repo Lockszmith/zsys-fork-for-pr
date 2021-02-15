d1dcd74ae5db303bf3cc3591069e66ddc7d1b08b
Lockszmith <github.zsys@lksz.me>
2021-02-14 23:57:08 -0500

    Added pager, auto detected based on term size

--- cmd/zsysd/client/docs.go
+++ cmd/zsysd/client/docs.go
@@ -1,10 +1,14 @@
 package client
 
 import (
-	"fmt"
+	//"bytes"
+	"golang.org/x/term"
 	"os"
+	"os/exec"
 	"strings"
 
+	//"github.com/alecthomas/chroma/quick"
+	"github.com/bbrks/wrap/v2"
 	"github.com/spf13/cobra"
 	"github.com/ubuntu/zsys/cmd/zsysd/cmdhandler"
 	"github.com/ubuntu/zsys/internal/i18n"
@@ -12,7 +16,7 @@ import (
 	_ "embed"
 )
 
-//go:embed whats-new.md
+//go:embed docs/whats-new.md
 var whatNewContent string
 
 var (
@@ -41,6 +45,40 @@ func renderDocs(topic string) error {
 		output = strings.ReplaceAll(whatNewContent, "#cmd#", "cmd")
 	}
 
-	_, err := os.Stdout.WriteString(fmt.Sprintf(i18n.G(output)))
+	width, height, err := term.GetSize(0)
+	if err != nil {
+		return err
+	}
+
+	w := wrap.NewWrapper()
+
+	// Prefix each new line with a single-line comment symbol.
+	w.OutputLinePrefix = "   "
+
+	renderedText := w.Wrap(i18n.G(output), width)
+	// buf := bytes.NewBufferString("")
+	// err = quick.Highlight(buf, renderedText, "markdown", "tty_truecolour", "monokai")
+	// renderedText = buf.String()
+
+	if len(renderedText) > (8 * width * height / 10) {
+		// Could read $PAGER rather than hardcoding the path.
+
+		pager := os.Getenv("PAGER")
+		if "" == pager {
+			pager = "/usr/bin/less"
+		}
+		cmd := exec.Command(pager)
+
+		// Feed it with the string you want to display.
+		cmd.Stdin = strings.NewReader(renderedText)
+
+		// This is crucial - otherwise it will write to a null device.
+		cmd.Stdout = os.Stdout
+
+		// Fork off a process and wait for it to terminate.
+		err = cmd.Run()
+	} else {
+		_, err = os.Stdout.WriteString(renderedText)
+	}
 	return err
 }
